906c0d8327712a513f015be8da935d94b12c4aeb
joker <uanhermanus@gmail.com>
2020-12-31 03:48:50 +0200

    create rough display container

---
+++ src/components/CounterDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="countdown">
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CounterDisplay",
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decremenr() {
+      this.count--;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.countdown {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+
+  font-size: 2em;
+}
+</style>
