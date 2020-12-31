67610d1efde16e3844d2e14e3121adb06965705a
joker <uanhermanus@gmail.com>
2020-12-31 03:49:04 +0200

    create flex containter for btns

---
+++ src/components/Row.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="btn-row">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Row",
+};
+</script>
+
+<style scoped>
+.btn-row {
+  display: flex;
+  justify-content: center;
+}
+</style>
