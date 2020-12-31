36a1e888dc1cbbbe687ea4b38f2bb4ad328d1685
joker <uanhermanus@gmail.com>
2020-12-31 03:49:40 +0200

    add imports; adjust css;add componets

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <CounterDisplay />
+  <!-- <div class="btn-row">
+  </div> -->
+
+  <Row>
+    <CounterButton title="+" />
+    <CounterButton title="-" />
+  </Row>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CounterDisplay from "./components/CounterDisplay";
+import CounterButton from "./components/ButtonCounter";
+import Row from "./components/Row";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    CounterDisplay,
+    CounterButton,
+    Row,
+  },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  height: 100vh;
 }
+
+/* .btn-row {
+  display: flex;
+  justify-content: center;
+} */
 </style>
